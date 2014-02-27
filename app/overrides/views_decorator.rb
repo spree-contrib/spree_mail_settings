543c452d1d68e36f81ea7b2bfcaf89fba6944005
John Hawthorn <john.hawthorn@gmail.com>
2014-02-27 16:41:00 -0500

    add override

---
+++ app/overrides/views_decorator.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(
+  :virtual_path => "spree/admin/shared/_configuration_menu",
+  :name => "mail_settings_admin_configurations_menu",
+  :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
+  :text => "<%= configurations_sidebar_menu_item Spree.t(:mail_method_settings), edit_admin_mail_method_url %>",
+  :disabled => false)
