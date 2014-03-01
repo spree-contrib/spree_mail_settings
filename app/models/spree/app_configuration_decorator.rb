529a386b7fe588aea500b3f73b822f7b92ed5ff9
John Hawthorn <john.hawthorn@gmail.com>
2014-02-28 20:32:25 -0500

    add app_configuration_decorator

---
+++ app/models/spree/app_configuration_decorator.rb
@@ -0,0 +1,17 @@
+module Spree
+  AppConfiguration.class_eval do
+    # Default mail headers settings
+    preference :enable_mail_delivery, :boolean, :default => false
+    preference :mail_bcc, :string, :default => 'spree@example.com'
+    preference :intercept_email, :string, :default => nil
+
+    # Default smtp settings
+    preference :mail_host, :string, :default => 'localhost'
+    preference :mail_domain, :string, :default => 'localhost'
+    preference :mail_port, :integer, :default => 25
+    preference :secure_connection_type, :string, :default => Core::MailSettings::SECURE_CONNECTION_TYPES[0]
+    preference :mail_auth_type, :string, :default => Core::MailSettings::MAIL_AUTH[0]
+    preference :smtp_username, :string
+    preference :smtp_password, :string
+  end
+end
