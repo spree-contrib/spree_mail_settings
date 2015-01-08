26a615a6c62abab26416bcae0a63600d384b5d32
Tobias Bohwalli <hi@futhr.io>
2015-01-08 07:29:50 +0100

    No need to inject empty manifest assets on install

--- lib/generators/spree_mail_settings/install/install_generator.rb
+++ lib/generators/spree_mail_settings/install/install_generator.rb
@@ -1,18 +1,7 @@
 module SpreeMailSettings
   module Generators
     class InstallGenerator < Rails::Generators::Base
-
-      class_option :auto_run_migrations, :type => :boolean, :default => false
-
-      def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_mail_settings\n"
-        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_mail_settings\n"
-      end
-
-      def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_mail_settings\n", :before => /\*\//, :verbose => true
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_mail_settings\n", :before => /\*\//, :verbose => true
-      end
+      class_option :auto_run_migrations, type: :boolean, default: false
 
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_mail_settings'
