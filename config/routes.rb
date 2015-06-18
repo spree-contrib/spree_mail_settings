6ff8c923f52c2b1fd62e73c637c32503c8a1b19e
gjselten <geert-jan@cg.nl>
2015-06-18 10:34:51 +0200

    better routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.routes.append do
+Spree::Core::Engine.add_routes do
   namespace :admin do
     resource :mail_method, only: [:edit, :update] do
       post :testmail, on: :collection
